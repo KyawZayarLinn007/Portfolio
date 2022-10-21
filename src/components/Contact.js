3f1134f6c2549a610a0fe433e139d347e20ffad3
KyawZayarLinn <kyawzayarlinnkb10@gmail.com>
2022-10-21 09:59:55 +0630

    emailjs init

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,38 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import EmailIcon from "@mui/icons-material/Email";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import SendIcon from "@mui/icons-material/Send";
 
-
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_4mmgt7s",
+        "template_3fdenri",
+        form.current,
+        "hSxOW3kppK1q0ubEk"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <Typography variant="h2">Contact</Typography>
@@ -23,24 +46,33 @@ const Contact = () => {
       >
         {/* contact detail section */}
         <Grid xs={12} md={6}>
-          <Typography variant="h6" sx={{ marginBottom: "4%", fontWeight: "bold" }}>
+          <Typography
+            variant="h6"
+            sx={{ marginBottom: "4%", fontWeight: "bold" }}
+          >
             You can contact me via
           </Typography>
 
           <Stack direction="row" spacing={1}>
             <EmailIcon />
-            <Typography variant="body1" sx={{ fontWeight: "bold" }}>kyawzayarlinnkb10@gmail.com</Typography>
+            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+              kyawzayarlinnkb10@gmail.com
+            </Typography>
           </Stack>
 
           <Stack direction="row" spacing={1}>
             <LocalPhoneIcon />
-            <Typography variant="body1" sx={{ fontWeight: "bold" }}>(+95)9787958124</Typography>
+            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+              (+95)9787958124
+            </Typography>
           </Stack>
         </Grid>
 
         {/* direct message section */}
         <Grid xs={12} md={6}>
-          <Typography variant="h6" sx={{ fontWeight: "bold" }}>Send me a direct message</Typography>
+          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+            Send me a direct message
+          </Typography>
 
           {/* form section */}
           <Box
@@ -48,29 +80,35 @@ const Contact = () => {
               marginTop: "4%",
             }}
           >
-            <Stack direction="row" spacing={6}>
-              {/* name field */}
-              <TextField id="name" label="Name" variant="standard" />
-              {/* email field */}
-              <TextField id="email" label="Email" variant="standard" />
-            </Stack>
+            <form ref={form} onSubmit={sendEmail}>
+              <Stack direction="row" spacing={6}>
+                {/* name field */}
+                <TextField id="user_name" name="user_name" label="Name" variant="standard" />
+                {/* email field */}
+                <TextField id="user_email" name="user_email" label="Email" variant="standard" />
+              </Stack>
 
-            {/* message field */}
-            <Stack direction="row" sx={{ marginTop: "2%", marginBottom: "4%" }}>
-              <TextField
-                id="message"
-                label="Message"
-                variant="standard"
-                rows={4}
-                multiline
-                sx={{width: "415px"}}
-              />
-            </Stack>
+              {/* message field */}
+              <Stack
+                direction="row"
+                sx={{ marginTop: "2%", marginBottom: "4%" }}
+              >
+                <TextField
+                  id="message"
+                  name="message"
+                  label="Message"
+                  variant="standard"
+                  rows={4}
+                  multiline
+                  sx={{ width: "415px" }}
+                />
+              </Stack>
 
-            {/* send btn */}
-            <Button variant="contained" endIcon={<SendIcon />}>
-              Send
-            </Button>
+              {/* send btn */}
+              <Button variant="contained" endIcon={<SendIcon />} type="submit">
+                Send
+              </Button>
+            </form>
           </Box>
         </Grid>
       </Grid>
